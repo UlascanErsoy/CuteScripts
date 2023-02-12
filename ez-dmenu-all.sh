2284d573bb7439b7acbd54de5116f1c97ff8f062
Ulascan Ersoy <ulascan@debian-BULLSEYE-live-builder-AMD64>
2023-02-11 23:06:41 -0600

    ez-dmenu-all.sh adds flatpak apps to dmenu

---
+++ ez-dmenu-all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash 
+#Script to add flatpak to the 
+#dmenu
+
+flatpak_apps=$(flatpak list)
+choices=""
+
+while IFS= read -r line; do 
+    if [[ $line =~ ^([^[[:space:]]*)[[:space:]]*([^[[:space:]]*) ]]; then
+       choices+="${BASH_REMATCH[1]}\n" 
+    fi
+
+done <<< "$flatpak_apps"
+
+choice=$(echo -e "$(dmenu_path)\n$choices" | dmenu)
+
+if [[ "$choices" =~ $choice ]]; then
+    while IFS= read -r line; do 
+        if [[ $line =~ ^$choice[[:space:]]*([^[[:space:]]*) ]]; then
+            flatpak run ${BASH_REMATCH[1]} &
+            exit 0
+        fi
+    done <<< "$flatpak_apps"
+else
+    echo $choice | ( ${SHELL:-"/bin/sh"} & ) 
+fi 
+
+
