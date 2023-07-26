7b766cb08e2386c041cbf77cebec36f90d071ec1
Ulascan Ersoy <cake@pop-os.localdomain>
2023-07-26 15:27:46 -0400

    pacmd to pactl

--- ez-audio-change.sh
+++ ez-audio-change.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-sinks=$(/usr/bin/pacmd list-sinks | grep -E 'index|device.description')
+sinks=$(/usr/bin/pactl list sinks | grep -E 'object.serial|Description')
 
 input_indexes=()
 choices=""
@@ -11,30 +11,31 @@ if ! [[ -z $1 ]]; then
     chooser_cmd=$1
 fi
 while IFS= read -r line; do 
-    if [[ $line =~  (index:[[:space:]])([0-9]+) ]]; then
-        choices+="${BASH_REMATCH[2]}-" 
-    elif [[ $line =~ (=[[:space:]])(.*) ]]; then
-        choices+=${BASH_REMATCH[2]}
+    if [[ $line =~ (Description:[[:space:]])(.*) ]]; then
+        choices+="${BASH_REMATCH[2]}-"
+    elif [[ $line =~ (object.serial[[:space:]]=[[:space:]]*)(.*) ]]; then
+        choices+="${BASH_REMATCH[2]}" 
         choices+="\n"
     fi
 done <<< "$sinks"
 
 choice=$(echo -e $(echo $choices | tr '"' ' ') | "$chooser_cmd")
 
-if [[ $choice =~ ([0-9]+)([-]) ]]; then
-    choice=${BASH_REMATCH[1]}
+if [[ $choice =~ ([-][[:space:]])([0-9]+) ]]; then
+    choice=${BASH_REMATCH[2]}
 fi
 
-/usr/bin/pacmd set-default-sink $choice
+/usr/bin/pactl set-default-sink $choice
 
 #if there are active audio sources switch them
 #manually
-inputs=$(/usr/bin/pacmd list-sink-inputs | grep 'index')
+inputs=$(/usr/bin/pactl list sink-inputs short)
 
 if ! [[ -z inputs ]]; then
     while IFS= read -r line; do 
-        if [[ $line =~  (index:[[:space:]])([0-9]+) ]]; then
-            /usr/bin/pacmd move-sink-input $choice ${BASH_REMATCH[2]}
+        if [[ $line =~  ([0-9]+)([[:space:]]) ]]; then
+            echo ${BASH_REMATCH[1]}
+            /usr/bin/pactl move-sink-input ${BASH_REMATCH[1]} $choice 
         fi 
             done <<< "$inputs" 
 fi 
