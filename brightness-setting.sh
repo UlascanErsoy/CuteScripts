d30070d15c18cc678df4365affe4c3be0e550663
UlascanErsoy <ulas.742@gmail.com>
2023-09-14 14:30:33 -0500

    you are my sunshine

---
+++ brightness-setting.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash 
+
+
+monitors=""
+index=0
+
+chooser="fzf"
+if ! [[ -z $1 ]];then
+    chooser=$1
+fi
+
+echo $chooser
+
+while IFS= read -r line; do
+    if [[ $line =~ [0-9][[:space:]](.*)[0-9][[:space:]]*(.+)$ ]]; then
+        monitors+="${BASH_REMATCH[2]}\n"
+        index+=1
+    fi
+done <<< $(xrandr --listmonitors)     
+
+if [ $index -gt 1 ]; then
+    monitor=$(echo -e $monitors | ${chooser})
+else
+    monitor="${BASH_REMATCH[2]}"
+fi
+
+choices="0.05\n0.1\n0.15\n0.2\n0.25\n0.3\n0.35\n0.4\n0.45\n0.5\n0.55\n0.6\n0.65\n0.7\n0.75\n0.8\n0.85\n0.9\n0.95\n1"
+
+choice=$(echo -e $choices | ${chooser})
+
+/usr/bin/xrandr --output $monitor --brightness $choice
--- monitor-update.sh
+++ monitor-update.sh
@@ -3,7 +3,8 @@
 HDMI=$(xrandr --listmonitors | grep HDMI)
 
 xrandr --auto
-xrandr --output HDMI-1-0 -r 100
+xrandr --output HDMI-1-0 --mode 3440x1440 --rate 100
+
 if ! [[ -z $HDMI ]]; then
     xrandr --auto --output eDP-1 --left-of HDMI-1-0
 fi 
